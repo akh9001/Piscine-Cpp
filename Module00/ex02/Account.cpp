45307f7e6ae9383e2b812ec9b7e127e3a7f1ab07
akh9001 <akhalidy9001@gmail.com>
2021-10-12 18:02:04 +0100

    ex02 done

--- Module00/ex02/Account.cpp
+++ Module00/ex02/Account.cpp
@@ -6,11 +6,12 @@
 /*   By: akhalidy <akhalidy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 20:22:51 by akhalidy          #+#    #+#             */
-/*   Updated: 2021/10/11 14:00:41 by akhalidy         ###   ########.fr       */
+/*   Updated: 2021/10/12 17:52:11 by akhalidy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
+#include <iostream>
 #include <ctime>
 #include <time.h>
 # define cin		std::cin
@@ -19,6 +20,7 @@
 # define string		std::string
 
 // Initialize static var
+
 int		Account::_nbAccounts = 0;
 int		Account::_totalAmount = 0;
 int		Account::_totalNbDeposits = 0;
@@ -28,53 +30,63 @@ int		Account::_totalNbWithdrawals = 0;
 
 Account::Account(int initial_deposit)
 {
+	// Set Acount var :
 	_accountIndex = _nbAccounts;
-	_totalAmount += initial_deposit;
-	_nbAccounts++;
 	_amount = initial_deposit;
 	_nbDeposits = 0;
 	_nbWithdrawals = 0;
+	// Set static var :
+	_totalAmount += initial_deposit;
+	_nbAccounts++;
+	// Display created account infos :
 	_displayTimestamp();
 	cout << "index:" << _accountIndex;
-	cout << "amount:" << _amount <<";created" << endl;
+	cout << ";amount:" << _amount <<";created" << endl;
 }
 
 Account::~Account(void)
 {
+	// Display distroyed account infos:
 	_displayTimestamp();
 	cout << "index:" << _accountIndex;
-	cout << "amount:" << _amount <<";closed" << endl;	
+	cout << ";amount:" << _amount <<";closed" << endl;	
 }
 
 int	Account::getNbAccounts( void )
 {
 	return (_nbAccounts);
 }
+
 int	Account::getTotalAmount( void )
 {
 	return (_totalAmount);
 }
+
 int	Account::getNbDeposits( void )
 {
 	return (_totalNbDeposits);
 }
+
 int	Account::getNbWithdrawals( void )
 {
 	return (_totalNbWithdrawals);
 }
+
 void	Account::displayStatus( void ) const
 {
-	cout << "[19920104_091532]" << "index:" << _accountIndex;
-	cout << "amount:" << _amount <<";created" << endl;
+	_displayTimestamp();
+	cout << "index:" << _accountIndex << ";amount:" << _amount;
+	cout << ";deposits:" << _nbDeposits << ";withdrawals:" << _nbWithdrawals;
+	cout << endl;
 }
 
 void	Account::displayAccountsInfos(void) // Static function ????
 {
-
+	_displayTimestamp();
 	cout << "accounts:" << _nbAccounts;
-	cout << "total:" << _totalAmount;
-	cout <<"deposits:" << _totalNbDeposits;
-	cout <<"withdrawals:" << _totalNbWithdrawals;
+	cout << ";total:" << _totalAmount;
+	cout <<";deposits:" << _totalNbDeposits;
+	cout <<";withdrawals:" << _totalNbWithdrawals;
 	cout << endl;
 }
 
@@ -93,7 +105,48 @@ void	Account::_displayTimestamp( void )
 
 void	Account::makeDeposit(int deposit)
 {
+	int	p_amount;
+
+	if (deposit <= 0)
+		return ;
+	// Set Acount var :
+	p_amount = _amount;
 	_amount += deposit;
+	_nbDeposits++;
+	// Set static var :
+	_totalAmount += deposit;
+	_totalNbDeposits++;
+	_displayTimestamp();
+	cout << "index:" << _accountIndex << ";p_amount:" << p_amount << ";deposit:" << deposit;
+	cout << ";amount:" << _amount << ";nb_deposits:" << _nbDeposits;
+	cout << endl;
 }
+
 bool	Account::makeWithdrawal( int withdrawal )
+{
+	int	p_amount;	
+
+	p_amount = _amount;
+	if (checkAmount() < withdrawal)
+	{
+		_displayTimestamp();
+		cout << "index:" << _accountIndex << ";p_amount:" << p_amount << ";withdrawal:refused";
+		cout << endl;
+		return (false);
+	}
+	_amount -= withdrawal;
+	_nbWithdrawals++;
+	// Set static var :
+	_totalAmount -= withdrawal;
+	_totalNbWithdrawals++;
+	_displayTimestamp();
+	cout << "index:" << _accountIndex << ";p_amount:" << p_amount << ";withdrawal:" << withdrawal;
+	cout << ";amount:" << _amount << ";nb_withdrawals:" << _nbWithdrawals;
+	cout << endl;
+	return (true);
+}
+
 int		Account::checkAmount( void ) const
+{
+	return (_amount);
+}
