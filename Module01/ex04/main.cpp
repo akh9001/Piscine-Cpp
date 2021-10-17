d87ff77a13690db4f13b3cfc18afb6a100742598
akh9001 <akhalidy9001@gmail.com>
2021-10-17 17:13:49 +0100

    ex04 Done

--- Module01/ex04/main.cpp
+++ Module01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: akhalidy <akhalidy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/15 09:31:54 by akhalidy          #+#    #+#             */
-/*   Updated: 2021/10/15 19:41:57 by akhalidy         ###   ########.fr       */
+/*   Updated: 2021/10/17 17:07:05 by akhalidy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,31 @@ void	ft_replace(string& line, string s1, string s2)
 	}
 }
 
+bool	ft_check_error(string filename, string s1, string s2, ifstream& ifs)
+{
+	if (!ifs.is_open())
+	{
+		cout << RED << "ERROR : Couldn't open "<< filename << WHITE << endl;	
+		return (true);
+	}
+	if (ifs.peek() == EOF)
+	{
+		cout << RED << "ERROR : Empty File !!" << WHITE << endl;
+		return (true);	
+	}
+	if (s1 == s2)
+	{
+		cout << RED << "Error: S1 && S2 are identical !" << WHITE << endl;
+		return (true);
+	}
+	if (!s1.length() || !s2.length())
+	{
+		cout << RED << "Error : Empty string" << WHITE << endl;
+		return (true);
+	}
+	return (false);
+}
+
 int	main(int argc, char **argv)
 {
 	string	filename;
@@ -59,15 +84,12 @@ int	main(int argc, char **argv)
 	s2 = string(argv[3]);
 	filename = string(argv[1]);
 	ifstream	ifs(filename);
-	ofstream	ofs(filename + ".replace");
-	if (!ifs.is_open())
-	{
-		cout << "ERROR : Couldn't open "<< filename <<" file!" << endl;	
+	if (ft_check_error(filename, s1, s2, ifs))
 		return (1);
-	}
+	ofstream	ofs(filename + ".replace");
 	if (!ofs.is_open())
 	{
-		cout << "ERROR : Couldn't open "<< filename + ".replace" <<" file!" << endl;	
+		cout << RED << "ERROR : Couldn't open "<< filename + ".replace" <<" file!" << WHITE << endl;	
 		return (1);		
 	}
 	while (!ifs.eof())
